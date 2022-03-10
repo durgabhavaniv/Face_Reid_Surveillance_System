b03f60b4014dcc2e605b7ee17fbc6547b9fb80c6
durga <durgabhavaniklu@gmail.com>
2022-03-10 13:22:40 +0530

    updates6

--- hackster/processVideo.sh
+++ hackster/processVideo.sh
@@ -2,4 +2,4 @@
 INPUTFILE=$1
 CONTAINERID=$(docker ps | grep -i 'xilinx/vitis-ai' | awk {'print $1'})
 
-docker exec -t --user vitis-ai-user $CONTAINERID bash -c "cd /workspace/demo/Vitis-AI-Library/samples/facedetect/ && source /workspace/setup/vck5000/setup.sh && ./facedetect.sh /workspace/demo/Vitis-AI-Library/input/$INPUTFILE"
+docker exec -t --user vitis-ai-user $CONTAINERID bash -c "cd /workspace/demo/Vitis-AI-Library/samples/facedetect/ && source /workspace/setup/vck5000/setup.sh && ./run.sh /workspace/demo/Vitis-AI-Library/input/$INPUTFILE"
